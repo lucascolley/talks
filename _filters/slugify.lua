d6358497b55bdd127d1ee05b2daef824bd65fb46
Lucas Colley <lucas.colley8@gmail.com>
2025-07-27 22:56:40 +0100

    setup for first talk dev

---
+++ _filters/slugify.lua
@@ -0,0 +1,11 @@
+-- Slugify titles for use in URLs or identifiers.
+local function slugify(s)
+    return s:gsub("[%s+/]+", "-"):gsub("[^%w-]+", ""):lower()
+end
+
+return {
+    Meta = function(m)
+        m.slug = m.title and slugify(pandoc.utils.stringify(m.title))
+        return m
+    end,
+}
