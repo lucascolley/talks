08f5065354622f3c77cded841512a1cca1178bab
Lucas Colley <lucas.colley8@gmail.com>
2025-08-09 16:25:49 +0100

    qr-code iter

--- _filters/slugify.lua
+++ _filters/slugify.lua
@@ -1,6 +1,14 @@
 -- Slugify titles for use in URLs or identifiers.
 local function slugify(s)
-    return s:gsub("[%s+/]+", "-"):gsub("[^%w-]+", ""):lower()
+    -- Replace spaces and slashes with dashes
+    s = s:gsub("[%s+/]+", "-")
+    -- Remove all non-ASCII characters (bytes > 0x7F)
+    s = s:gsub("[^\0-\127]", "")
+    -- Remove anything not alphanumeric or dash
+    s = s:gsub("[^%w-]+", "")
+    -- Lowercase
+    s = s:lower()
+    return s
 end
 
 return {
